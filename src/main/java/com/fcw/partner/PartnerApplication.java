98c45f44b8acc79c63d9441315178c5e92d2e141
chengwu <2477376240@qq.com>
2024-04-26 19:30:55 +0800

    添加返回简介

--- src/main/java/com/fcw/partner/PartnerApplication.java
+++ src/main/java/com/fcw/partner/PartnerApplication.java
@@ -3,9 +3,11 @@ package com.fcw.partner;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.fcw.partner.mapper")
+@EnableScheduling
 public class PartnerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PartnerApplication.class, args);
